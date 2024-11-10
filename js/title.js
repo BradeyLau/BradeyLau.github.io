74911b3425720fbd4cef1cd66343ce164ffef650
BradeyLau <zer0breathe5@gmail.com>
2024-11-10 14:25:22 +0800

    Site updated: 2024-11-10 14:25:21

--- js/title.js
+++ js/title.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '👀不要离开我😭';
+    document.title = '不要离开我😭';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🐖等你好久啦';
+    document.title = '等你好久啦😁';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
